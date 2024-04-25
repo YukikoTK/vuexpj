e2eb11a0d2c45817c11e6bd2ed2e9a0568e14c8b
YukikoTK <remo1ey1yukk@yahoo.co.jp>
2024-04-25 22:25:01 +0900

    練習を追加

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 1
   },
   getters: {
+    dubble: (state, getters) => {
+      console.log(getters);
+      return state.count * 2;
+    }
   },
   mutations: {
+    incrementMutation(state) {
+      state.count++
+    }
   },
   actions: {
+    incrementAction(context) {
+      setTimeout(function () { // １秒かかる通信
+        let data = 1;
+        console.log("waited a second");
+        context.commit("incrementMutation",data);
+      }, 1000);
+    },
+},
   },
   modules: {
   }
